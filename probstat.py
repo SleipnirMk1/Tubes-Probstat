77a5c73b710722a0ea7370f08e1b3ccbab253c66
SleipnirMk1 <ilyasalefi@gmail.com>
2021-04-15 23:04:32 +0700

    korelasi

--- probstat.py
+++ probstat.py
@@ -22,7 +22,9 @@ df.kurt()
 #Mengetahui distribusi normal dapat dengan membandingkan bentuk histogram dengan normal probability curve
 #Daerah
 df.hist(column="Daerah")
+
 df.boxplot(column="Daerah")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["Daerah"], dist="norm", plot=pylab)
 pylab.show()
@@ -30,7 +32,9 @@ print("Kesimpulan: Berdistribusi normal")
 
 #Sumbu Utama
 df.hist(column="SumbuUtama")
+
 df.boxplot(column="SumbuUtama")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["SumbuUtama"], dist="norm", plot=pylab)
 pylab.show()
@@ -38,7 +42,9 @@ print("Kesimpulan: Tidak berdistribusi normal, terdapat outlier yang signifikan"
 
 #Sumbu Kecil
 df.hist(column="SumbuKecil")
+
 df.boxplot(column="SumbuKecil")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["SumbuKecil"], dist="norm", plot=pylab)
 pylab.show()
@@ -46,7 +52,9 @@ print("Kesimpulan: Berdistribusi normal")
 
 #Keunikan
 df.hist(column="Keunikan")
+
 df.boxplot(column="Keunikan")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["Keunikan"], dist="norm", plot=pylab)
 pylab.show()
@@ -54,7 +62,9 @@ print("Kesimpulan: Tidak berdistribusi normal")
 
 #Area Bulatan
 df.hist(column="AreaBulatan")
+
 df.boxplot(column="AreaBulatan")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["AreaBulatan"], dist="norm", plot=pylab)
 pylab.show()
@@ -62,7 +72,9 @@ print("Kesimpulan: Berdistribusi normal")
 
 #Diameter
 df.hist(column="Diameter")
+
 df.boxplot(column="Diameter")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["Diameter"], dist="norm", plot=pylab)
 pylab.show()
@@ -70,7 +82,9 @@ print("Kesimpulan: Berdistribusi normal")
 
 #Kadar Air
 df.hist(column="KadarAir")
+
 df.boxplot(column="KadarAir")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["KadarAir"], dist="norm", plot=pylab)
 pylab.show()
@@ -78,7 +92,9 @@ print("Kesimpulan: Tidak berdistribusi normal")
 
 #Keliling
 df.hist(column="Keliling")
+
 df.boxplot(column="Keliling")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["Keliling"], dist="norm", plot=pylab)
 pylab.show()
@@ -86,7 +102,9 @@ print("Kesimpulan: Tidak berdistribusi normal")
 
 #Bulatan
 df.hist(column="Bulatan")
+
 df.boxplot(column="Bulatan")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["Bulatan"], dist="norm", plot=pylab)
 pylab.show()
@@ -94,8 +112,85 @@ print("Kesimpulan: Tidak berdistribusi normal")
 
 #Ransum
 df.hist(column="Ransum")
+
 df.boxplot(column="Ransum")
+
 print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
 stats.probplot(df["Ransum"], dist="norm", plot=pylab)
 pylab.show()
 print("Kesimpulan: Tidak berdistribusi normal")
+
+#Kelas
+df.hist(column="Kelas")
+
+df.boxplot(column="Kelas")
+
+print("Pengujian distribusi normal dengan normality test Quartile-Quartile plot: ")
+stats.probplot(df["Kelas"], dist="norm", plot=pylab)
+pylab.show()
+print("Kesimpulan: Tidak berdistribusi normal")
+
+#Korelasi
+#Target adalah kolom "Kelas", yang memiliki nilai 1 atau 2
+print("Correlations")
+
+#Kelas-Daerah
+df.plot.scatter("Kelas", "Daerah")
+
+print("Korelasi negatif kuat (0.6-0.79)")
+df["Kelas"].corr(df["Daerah"])
+
+#Kelas-SumbuUtama
+df.plot.scatter("Kelas", "SumbuUtama")
+
+print("Korelasi negatif kuat (0.6-0.79)")
+df["Kelas"].corr(df["SumbuUtama"])
+
+#Kelas-SumbuKecil
+df.plot.scatter("Kelas", "SumbuKecil")
+
+print("Korelasi negatif sangat lemah (0.0-0.19)")
+df["Kelas"].corr(df["SumbuKecil"])
+
+#Kelas-Keunikan
+df.plot.scatter("Kelas", "Keunikan")
+
+print("Korelasi negatif kuat (0.6-0.79)")
+df["Kelas"].corr(df["Keunikan"])
+
+#Kelas-AreaBulatan
+df.plot.scatter("Kelas", "AreaBulatan")
+
+print("Korelasi negatif kuat (0.6-0.79)")
+df["Kelas"].corr(df["AreaBulatan"])
+
+#Kelas-Diameter
+df.plot.scatter("Kelas", "Diameter")
+
+print("Korelasi negatif kuat (0.6-0.79)")
+df["Kelas"].corr(df["Diameter"])
+
+#Kelas-KadarAir
+df.plot.scatter("Kelas", "KadarAir")
+
+print("Korelasi positif sangat lemah (0.0-0.19)")
+df["Kelas"].corr(df["KadarAir"])
+
+#Kelas-Keliling
+df.plot.scatter("Kelas", "Keliling")
+
+print("Korelasi negatif kuat (0.6-0.79)")
+df["Kelas"].corr(df["Keliling"])
+
+#Kelas-Bulatan
+df.plot.scatter("Kelas", "Bulatan")
+
+print("Korelasi positif sedang (0.4-0.59)")
+df["Kelas"].corr(df["Bulatan"])
+
+#Kelas-Ransum
+df.plot.scatter("Kelas", "Ransum")
+
+print("Korelasi negatif sangat kuat (0.8-1.0)")
+df["Kelas"].corr(df["Ransum"])
+
